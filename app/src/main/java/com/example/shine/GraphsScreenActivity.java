4020c84bbadad40d7ac85e632ff0121b0ce1ab4c
amalik23 <amalik23@wisc.edu>
2021-12-04 17:10:05 -0600

    adding GraphScreenActivity

---
+++ app/src/main/java/com/example/shine/GraphsScreenActivity.java
@@ -0,0 +1,14 @@
+package com.example.shine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GraphsScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graphs_screen);
+    }
+}
