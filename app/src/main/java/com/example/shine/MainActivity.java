8772408d07ecf0cf046aa22980d7a53b2b45159b
raahaazfar <raahaazfar@gmail.com>
2021-12-06 17:06:33 -0600

    Update MainActivity.java

--- app/src/main/java/com/example/shine/MainActivity.java
+++ app/src/main/java/com/example/shine/MainActivity.java
@@ -4,7 +4,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -140,4 +144,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ForgotPasswordActivity.class);
         startActivity(intent);
     }
+
+
+
 }
