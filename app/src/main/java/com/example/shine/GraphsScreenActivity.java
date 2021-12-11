bb7a9f898f8e9b9c6edec1dc2e97067650bb360f
amalik23 <amalik23@wisc.edu>
2021-12-11 12:35:28 -0600

    Updates to get firebase data

--- app/src/main/java/com/example/shine/GraphsScreenActivity.java
+++ app/src/main/java/com/example/shine/GraphsScreenActivity.java
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.data.PieEntry;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -17,6 +19,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.lang.reflect.Array;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.Year;
@@ -30,17 +33,19 @@ public class GraphsScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graphs_screen);
-        db = FirebaseFirestore.getInstance();
-        pieChart();
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
-    private void pieChart(){
+    public void pieChart(View view){
+        db = FirebaseFirestore.getInstance();
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         LocalDate past = LocalDate.now().minusMonths(1);
         Month month = past.getMonth();
         int year = past.getYear();
         ArrayList<PieEntry> entries = new ArrayList<>();
+        ArrayList<Transaction> transactions = new ArrayList<>();
+        final QueryDocumentSnapshot[] document1 = new QueryDocumentSnapshot[1];
+        final String[] id = new String[1];
         db.collection("users").document(user.getUid()).collection(month.toString()+"-"+year)
             .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
@@ -48,13 +53,14 @@ public class GraphsScreenActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
                             for (QueryDocumentSnapshot document : task.getResult()) {
-                                entries.add(new PieEntry( (float) document.get("amount"), document.get("category")));
-                                //Log.d(TAG, document.getId() + " => " + document.getData());
+                                Transaction transaction = document.toObject(Transaction.class);
+                                transactions.add(transaction);
                             }
                         } else {
                             //Log.d(TAG, "Error getting documents: ", task.getException());
                         }
                     }
                 });
+        ArrayList<PieEntry> entries1 = entries;
     }
 }
