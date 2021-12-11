fc3610a0025944197f35ec52ee9d66739832ca43
h-ayub <ayub.humza@gmail.com>
2021-12-11 11:49:22 -0600

    Finished Settings Screen User can change name, password, email so long as they verify their password

--- app/src/main/java/com/example/shine/SettingsActivity.java
+++ app/src/main/java/com/example/shine/SettingsActivity.java
@@ -1,6 +1,5 @@
 package com.example.shine;
 
-import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
@@ -10,7 +9,7 @@ import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -30,6 +29,8 @@ public class SettingsActivity extends AppCompatActivity {
     private EditText emailEditText, nameEditText;
     private FirebaseAuth mAuth;
     private SharedPreferences sharedPreferences;
+    private AlertDialog dialog;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,73 +64,87 @@ public class SettingsActivity extends AppCompatActivity {
         String uid = sharedPreferences.getString(MainActivity.uidKey, "");
         User user = new User(sharedPreferences.getString(MainActivity.nameKey, ""), sharedPreferences.getString(MainActivity.emailKey, ""));
 
-        if (!emailBool) {
-            String email = emailEditText.getText().toString().trim();
-            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                emailEditText.setError("Please provide valid email!");
-                emailEditText.requestFocus();
+        popup();
+        dialog.setOnDismissListener(dialogInterface -> {
+            if (password.equals(""))
                 return;
-            }
-            firebaseUser.updateEmail(email).addOnCompleteListener(task -> {
-                Drawable checkmark = ContextCompat.getDrawable(SettingsActivity.this, R.drawable.ic_baseline_check_24);
-                checkmark.setBounds(0, 0, checkmark.getIntrinsicWidth(), checkmark.getIntrinsicHeight());
-                emailEditText.setError("Please validate email address", checkmark);
-                emailEditText.requestFocus();
-                firebaseUser.sendEmailVerification();
-                user.setEmail(email);
-                sharedPreferences.edit().putString(MainActivity.emailKey, email).apply();
+            mAuth.signInWithEmailAndPassword(sharedPreferences.getString(MainActivity.emailKey, ""), password).addOnSuccessListener(authResult -> {
+
+
+                if (!nameBool) {
+                    String name =  nameEditText.getText().toString().trim();
+                    user.setName(name);
+                    sharedPreferences.edit().putString(MainActivity.nameKey, name).apply();
+                    Toast.makeText(SettingsActivity.this, "Name has been changed", Toast.LENGTH_LONG).show();
+                    db.collection("users").document(uid).set(user);
+                }
+
+                if (!emailBool) {
+                    String email = emailEditText.getText().toString().trim();
+                    if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                        emailEditText.setError("Please provide valid email!");
+                        emailEditText.requestFocus();
+                        return;
+                    }
+
+                    firebaseUser.updateEmail(email).addOnSuccessListener(task -> {
+                        user.setEmail(email);
+                        sharedPreferences.edit().putString(MainActivity.emailKey, email).apply();
+                        firebaseUser.sendEmailVerification();
+                        Toast.makeText(SettingsActivity.this, "Check new email to verify address", Toast.LENGTH_LONG).show();
+                        db.collection("users").document(uid).set(user);
+                    }).addOnFailureListener(e -> {
+                        emailEditText.setError("An error occurred please try again!");
+                        emailEditText.requestFocus();
+                    });
+                }
             }).addOnFailureListener(e -> {
-                emailEditText.setError("An error occurred please try again!");
-                emailEditText.requestFocus();
+                Toast.makeText(SettingsActivity.this, "Password incorrect!", Toast.LENGTH_LONG).show();
             });
-        }
+        });
+}
 
-        if (!nameBool) {
-            String name =  nameEditText.getText().toString().trim();
-            user.setName(name);
-            sharedPreferences.edit().putString(MainActivity.nameKey, name).apply();
-        }
-
-        db.collection("users").document(uid).set(user);
-    }
-
-    public void changePassword(View view) {
+    private void popup() {
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this).setNeutralButton("Confirm", null);
 
         final View passwordPopupView = getLayoutInflater().inflate(R.layout.password_popup, null);
 
         dialogBuilder.setView(passwordPopupView);
-        AlertDialog dialog = dialogBuilder.create();
+        dialog = dialogBuilder.create();
 
         dialog.setButton(AlertDialog.BUTTON_POSITIVE, "Dismiss",
-                ((dialogInterface, i1) -> dialogInterface.dismiss()));
+                ((dialogInterface, i1) -> {
+                    password = "";
+                    dialogInterface.dismiss();}));
 
         dialog.setOnShowListener(dialogInterface -> {
             Button confirm = dialog.getButton(AlertDialog.BUTTON_NEUTRAL);
             confirm.setOnClickListener(v -> {
                 EditText passwordEditText = passwordPopupView.findViewById(R.id.editTextPopupPassword);
-                String password = passwordEditText.getText().toString();
-
+                password = passwordEditText.getText().toString();
                 if (password.isEmpty()) {
                     passwordEditText.setError("Password is required");
                     passwordEditText.requestFocus();
                     return;
                 }
 
-                mAuth.signInWithEmailAndPassword(sharedPreferences.getString(MainActivity.emailKey, ""), password).addOnSuccessListener(authResult -> {
-                    mAuth.sendPasswordResetEmail(sharedPreferences.getString(MainActivity.emailKey,""));
-                    Drawable checkmark = ContextCompat.getDrawable(SettingsActivity.this, R.drawable.ic_baseline_check_24);
-                    checkmark.setBounds(0, 0, checkmark.getIntrinsicWidth(), checkmark.getIntrinsicHeight());
-                    passwordEditText.setError("Check email to reset password", checkmark);
-                    passwordEditText.requestFocus();
-                }).addOnFailureListener(e -> {
-                    passwordEditText.setError("Password incorrect!");
-                    passwordEditText.requestFocus();
-                });
-                // dialog.dismiss();
+                dialog.dismiss();
             });
         });
 
         dialog.show();
     }
+
+    public void changePassword(View view) {
+        popup();
+        dialog.setOnDismissListener(dialogInterface -> {
+            if (password.equals(""))
+                return;
+            mAuth.signInWithEmailAndPassword(sharedPreferences.getString(MainActivity.emailKey, ""), password).addOnSuccessListener(authResult -> {
+            Toast.makeText(SettingsActivity.this, "Check email to reset password", Toast.LENGTH_LONG).show();
+            mAuth.sendPasswordResetEmail(sharedPreferences.getString(MainActivity.emailKey, ""));
+            }).addOnFailureListener(e -> Toast.makeText(SettingsActivity.this, "Password incorrect!", Toast.LENGTH_LONG).show());
+        });
+    }
+
 }
